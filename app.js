2c9fc5123fe0f85fa58381043d226d43621d906c
robfree <robertoserrano83@gmail.com>
2015-07-02 16:30:29 +0200

    Marco de la aplicación

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var partials = require('express-partials');
 
 var routes = require('./routes/index');
 
@@ -13,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+app.use(partials());
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,14 @@
 // GET /quizes/question
 exports.question = function(req, res) {
-  res.render('/quizes/question', {pregunta: 'Capital de Italia'});
+  res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
   if (req.query.respuesta === 'Roma') {
-    res.render('/quizes/answer', {respuesta: 'Correcto'});
+    res.render('quizes/answer', {respuesta: 'Correcto'});
   }
   else {
-    res.render('/quizes/answer', {respuesta: 'Incorrecto'});
+    res.render('quizes/answer', {respuesta: 'Incorrecto'});
   }
 };

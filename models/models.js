91bf0e4feeb5f6cfcd7172dcdea093d8810b97b5
robfree <robertoserrano83@gmail.com>
2015-07-29 21:37:10 +0200

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,42 @@
+var path = require('path');
+
+// Postgres DATABASE_URL = postgres://user:passwd@host:post/database
+// SQlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name   = (url[6] || null);
+var user      = (url[2] || null);
+var pwd       = (url[3] || null);
+var protocol  = (url[1] || null);
+var dialect   = (url[1] || null);
+var port      = (url[5] || null);
+var host      = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQlite
+
+var sequelize = new Sequelize(DB_name, user, pwd,
+                      { dialect: protocol,
+                        protocol: protocol,
+                        port: port,
+                        host: host,
+                        storage: storage, // solo SQlite (.env)
+                        omitNull: true    // solo Psotgres
+                      }
+                    );
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz;  // exportar la definicion de tabla Quiz
+
+// sequelize.sync() crea e inicilaliza tabla de preguntas
+sequelize.sync();
+Quiz.count().then(function(count){
+  if (count === 0) {
+    Quiz.create( {pregunta: 'Capital de Italia', respuesta: 'Roma'} ).then(function(){
+      console.log('Base de Datos inicializada');
+    });
+  };
+});
